1de24c2ad8376ed060576f17f72c4156c7053316
omkar <shelkeom230@gmail.com>
2024-03-23 19:06:49 +0530

    project 1 counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [counter,setCounter]=useState(15)
+  // let counter=15
+
+
+  const addValue=()=>{
+    setCounter((prev)=>prev+1)
+    setCounter((prev)=>prev+1)
+    setCounter((prev)=>prev+1)
+    setCounter((prev)=>prev+1)
+  }
+
+  const removeValue=()=>{
+    setCounter((prev)=>prev-1)
+    setCounter((prev)=>prev-1)
+    setCounter((prev)=>prev-1)
+    setCounter((prev)=>prev-1)
+  }
+  return (
+    <>
+      <h1>React course with Hitesh {counter}</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value</button> {"  "}  
+      <button onClick={removeValue}>Remove value</button>  
+      <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
